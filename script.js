ed7c0f5a2204e9fe46d7e50cc72225c25d78f6c3
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-29 20:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+
+function getNumbers() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+
+function filterEvenNumbers(numbers) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const evenNumbers = numbers.filter((num) => num % 2 === 0);
+      document.getElementById("output").textContent = evenNumbers.join(", "); 
+      resolve(evenNumbers);
+    }, 1000); 
+  });
+}
+
+function multiplyByTwo(numbers) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const multipliedNumbers = numbers.map((num) => num * 2);
+      document.getElementById("output").textContent = multipliedNumbers.join(", "); 
+      resolve(multipliedNumbers);
+    }, 2000); 
+  });
+}
+
+getNumbers()
+  .then(filterEvenNumbers) 
+  .then(multiplyByTwo)     
+  .catch((error) => {
+    console.error("An error occurred:", error); 
+  });
